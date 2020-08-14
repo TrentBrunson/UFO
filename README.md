e8626ebf0a5c5ca7967f5f725c45b3de3c599f8d
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-08-14 15:28:00 -0500

    updating readme

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 --- 
 Requestor provided a database of reported UFO sitings.  Built a front end with HTML, CSS & JavaScript to allow users to have a dynamic filter to select data from the dataset.  Converted all input cases to match lowercase database strings.
 
+Requestor asked for if-else statements.  Including else in JS broke the logic of the multi-filter selections.  Returned results needed AND logic for the conditions.  If-else sequenced through the filter and will stop at the first returned value.
+
 ### Resources  
 ---  
 JavaScript  
--- static/js/test.js
+++ static/js/test.js
@@ -1,56 +1,17 @@
 
-    // // Save the element, value, and id of the filter that was changed
-    // let selectedDate = d3.select("#datetime").property("value");
-    // let selectedCity = d3.select("#city").property("value").toLowerCase();
-    // let selectedState = d3.select("#state").property("value").toLowerCase();
-    // let selectedCountry = d3.select("#country").property("value").toLowerCase();
-    // let selectedShape = d3.select("#shape").property("value").toLowerCase();
+    // Save the element, value, and id of the filter that was changed
+    let selectedDate = d3.select("#datetime").property("value");
+    let selectedCity = d3.select("#city").property("value").toLowerCase();
+    let selectedState = d3.select("#state").property("value").toLowerCase();
+    let selectedCountry = d3.select("#country").property("value").toLowerCase();
+    let selectedShape = d3.select("#shape").property("value").toLowerCase();
 
-    // let filteredCards = ufo.filter(function(){
-    //     return ufo.date == selectedDate
-    //         && ufo.city == selectedCity
-    //         && ufo.state == selectedState
-    //         && ufo.country == selectedCountry
-    //         && ufo.shape == selectedShape;
-    // });
-
-    // ---------------**************************
-    
-
-    // import data from data.js
-const tableData = data;
-
-// reference the HTML table using d3
-var tbody = d3.select("tbody");
-
-function buildTable(data) {
-    tbody.html("");
-    data.forEach((dataRow) => {
-        let row = tbody.append("tr");
-        Object.values(dataRow).forEach((val) => {
-            let cell = row.append("td");
-            cell.text(val);
-            }
-        );
+    let filteredCards = ufo.filter(function(){
+        return ufo.date == selectedDate
+            && ufo.city == selectedCity
+            && ufo.state == selectedState
+            && ufo.country == selectedCountry
+            && ufo.shape == selectedShape;
     });
-}
 
-function cityFilter() {
-    // grab datetime from the filter
-    let city = d3.select("#city").property("value").toLowerCase();
-    let filteredData = tableData;
-
-    // check for user-selected datetime in tableData
-    if (city) {
-        filteredData = filteredData.filter(row => row.city === city);
-    }
     
-    // rebuild table using filtered data
-    buildTable(filteredData);
-}   
-
-// listen for click event
-d3.selectAll("#filter-btn").on("click", cityFilter);
-
-// build table when page loads
-buildTable(tableData);
