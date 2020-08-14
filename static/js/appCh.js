2041da5ec51e1b0a0c2b2c7d7ad606e870600375
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-08-14 15:04:28 -0500

    fixed html bug; all filters working now

--- static/js/appCh.js
+++ static/js/appCh.js
@@ -16,9 +16,6 @@ function buildTable(data) {
     });
 }
 
-// Keep track of all filters
-// var filters = {};
-
 function updateFilters() {
     // Save the element, value, and id of the filter that was changed
     let date = d3.select("#datetime").property("value");
@@ -29,32 +26,15 @@ function updateFilters() {
     let filteredData = tableData;
 
     // check for user-selected filters in tableData
-    if(date) {filteredData = filteredData.filter(row => row.datetime === date);} 
-    if(city) {filteredData = filteredData.filter(row => row.city === city);} 
-    if(state) {filteredData=filteredData.filter(row => row.state === state);} 
-    if(country) {filteredData = filteredData.filter(row => row.country === country);} 
-    if(shape) {filteredData = filteredData.filter(row => row.shape === shape);} 
-
-    // Call function to apply all filters and rebuild the table
-    // filterTable(filteredData);
+    if(date){filteredData = filteredData.filter(row => row.datetime === date);} 
+    if(city){filteredData = filteredData.filter(row => row.city === city);} 
+    if(state){filteredData=filteredData.filter(row => row.state === state);} 
+    if(country){filteredData = filteredData.filter(row => row.country === country);} 
+    if(shape){filteredData = filteredData.filter(row => row.shape === shape);} 
 
     // rebuild table using filtered data
     buildTable(filteredData);
-}   
-function filterTable() {
-
-    // Set the filteredData to the tableData
-    let filteredData = tableData;
-
-    // Loop through all of the filters and keep any data that matches the filter values
-    // if date && city && state && country && shape ==
-    // 
-
-  
-    // Finally, rebuild the table using the filtered Data
-    buildTable(filteredData);
-  }
-
+}  
 // listen for click event
 d3.selectAll("#filter-btn").on("click", updateFilters);
 
