f3ad41a3c34cefe274a4d825f62b987e4a629e67
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-08-14 14:52:25 -0500

    test other filters

--- static/js/appCh.js
+++ static/js/appCh.js
@@ -16,8 +16,8 @@ function buildTable(data) {
     });
 }
 
-// // Keep track of all filters
-var filters = {};
+// Keep track of all filters
+// var filters = {};
 
 function updateFilters() {
     // Save the element, value, and id of the filter that was changed
@@ -26,43 +26,30 @@ function updateFilters() {
     let state = d3.select("#state").property("value").toLowerCase();
     let country = d3.select("#country").property("value").toLowerCase();
     let shape = d3.select("#shape").property("value").toLowerCase();
+    let filteredData = tableData;
 
-    // check for user-selected datetime in tableData
-    if (date) {
-        filteredData = filteredData.filter(row => row.datetime === date);
-    } else {filteredData = filteredData}
-    
-    if(city) {
-        filteredData = filteredData.filter(row => row.city === city);
-    } else {filteredData = filteredData}
-
-    if(state) {
-        filteredData=filteredData.filter(row => row.state === state);
-    } else {filteredData = filteredData}
-
-    if(country) {
-        filteredData = filteredData.filter(row => row.country === country);
-    } else {filteredData = filteredData}
-
-    if(shape) {
-        filteredData = filteredData.filter(row => row.shape === shape);
-    } else {filteredData = filteredData}
+    // check for user-selected filters in tableData
+    if(date) {filteredData = filteredData.filter(row => row.datetime === date);} 
+    if(city) {filteredData = filteredData.filter(row => row.city === city);} 
+    if(state) {filteredData=filteredData.filter(row => row.state === state);} 
+    if(country) {filteredData = filteredData.filter(row => row.country === country);} 
+    if(shape) {filteredData = filteredData.filter(row => row.shape === shape);} 
 
     // Call function to apply all filters and rebuild the table
-    filterTable(filteredData);
+    // filterTable(filteredData);
 
     // rebuild table using filtered data
     buildTable(filteredData);
 }   
-
 function filterTable() {
 
     // Set the filteredData to the tableData
     let filteredData = tableData;
 
-    // Loop through all of the filters and keep any data that
-    // matches the filter values
-  
+    // Loop through all of the filters and keep any data that matches the filter values
+    // if date && city && state && country && shape ==
+    // 
+
   
     // Finally, rebuild the table using the filtered Data
     buildTable(filteredData);
@@ -73,3 +60,10 @@ d3.selectAll("#filter-btn").on("click", updateFilters);
 
 // build table when page loads
 buildTable(tableData);
+
+
+
+
+
+
+
