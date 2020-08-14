7570b369ec5898b281f4ca0e4953caed90204719
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-08-14 15:06:19 -0500

    fixed html bug; all filters working now

--- static/js/app.js
+++ static/js/app.js
@@ -16,22 +16,34 @@ function buildTable(data) {
     });
 }
 
-function handleClick() {
-    // grab datetime from the filter
+function updateFilters() {
+    // Save the element, value, and id of the filter that was changed
     let date = d3.select("#datetime").property("value");
+    let city = d3.select("#city").property("value").toLowerCase();
+    let state = d3.select("#state").property("value").toLowerCase();
+    let country = d3.select("#country").property("value").toLowerCase();
+    let shape = d3.select("#shape").property("value").toLowerCase();
     let filteredData = tableData;
 
-    // check for user-selected datetime in tableData
-    if (date) {
-        filteredData = filteredData.filter(row => row.datetime === date);
-    }
-    
+    // check for user-selected filters in tableData
+    if(date){filteredData = filteredData.filter(row => row.datetime === date);} 
+    if(city){filteredData = filteredData.filter(row => row.city === city);} 
+    if(state){filteredData=filteredData.filter(row => row.state === state);} 
+    if(country){filteredData = filteredData.filter(row => row.country === country);} 
+    if(shape){filteredData = filteredData.filter(row => row.shape === shape);} 
+
     // rebuild table using filtered data
     buildTable(filteredData);
-}   
-
+}  
 // listen for click event
-d3.selectAll("#filter-btn").on("click", handleClick);
+d3.selectAll("#filter-btn").on("click", updateFilters);
 
 // build table when page loads
 buildTable(tableData);
+
+
+
+
+
+
+
